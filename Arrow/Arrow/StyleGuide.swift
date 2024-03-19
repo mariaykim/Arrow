c23c553c3a76f44189268a9b337d0caeeee634a6
Maria Kim <80354714+mariaykim@users.noreply.github.com>
2024-03-19 16:45:59 -0400

    create styleguide constants file

---
+++ Arrow/Arrow/StyleGuide.swift
@@ -0,0 +1,19 @@
+//
+//  StyleGuide.swift
+//  Arrow
+//
+//  Created by Maria Kim on 3/19/24.
+//
+
+import Foundation
+
+struct StyleGuide {
+    
+    enum Size {
+        static var extraSmallPadding = 5
+        static var smallPadding = 10
+        static var mediumPadding = 20
+        static var largePadding = 30
+    }
+    
+}
