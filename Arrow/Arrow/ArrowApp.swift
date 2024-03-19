0bd1dfd32ddf0ba11c81b6338e1613fcff36ad21
Maria Kim <80354714+mariaykim@users.noreply.github.com>
2024-03-19 16:41:44 -0400

    initial commit

---
+++ Arrow/Arrow/ArrowApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArrowApp.swift
+//  Arrow
+//
+//  Created by Maria Kim on 3/19/24.
+//
+
+import SwiftUI
+
+@main
+struct ArrowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
